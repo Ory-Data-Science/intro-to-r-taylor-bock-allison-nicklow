a7d0da3c335677b0cfe04fd9e43528f385b865e0
tay bock <taylor.bock@stlcop.edu>
2017-10-14 14:32:20 -0500

    added the comment of problem 6

--- 6.R
+++ 6.R
@@ -1,3 +1,4 @@
+#This is the average precipitation for each month across years.
 ppt_data <- read.csv("gainesville-precip.csv", header = FALSE)
 monthly_mean_ppt <- colMeans(ppt_data)
 plot(monthly_mean_ppt, type = "l", xlab = "Month", ylab = "Mean Precipitation")
