96ba4c6cd41f7a6d7c9dee43e3a5c31d9b83cea0
tay bock <taylor.bock@stlcop.edu>
2017-10-14 14:04:08 -0500

    problem 5

---
+++ 5.R
@@ -0,0 +1,18 @@
+site1_g_carbon_m2_day <- 5
+site2_g_carbon_m2_day <- 2.3
+site1_area_m2 <- 200
+site2_area_m2 <- 450
+site1_npp_day <- site1_g_carbon_m2_day * site1_area_m2
+site2_npp_day <- site2_g_carbon_m2_day * site2_area_m2
+site1_g_carbon_m2_day <- 5
+site2_g_carbon_m2_day <- 2.3
+site1_area_m2 <- 200
+site2_area_m2 <- 450
+site1_npp_day <- site1_g_carbon_m2_day * site1_area_m2
+site2_npp_day <- site2_g_carbon_m2_day * site2_area_m2
+print(site1_npp_day)
+print(site2_npp_day)
+sum(site1_npp_day, site2_npp_day)
+abs(site1_npp_day, site2_npp_day)
+abs(site1_npp_day - site2_npp_day)
+
