7e35232b3a742fb26b44a453c1007c0c273a2583
tay bock <taylor.bock@stlcop.edu>
2017-10-14 14:30:58 -0500

    6

---
+++ 6.R
@@ -0,0 +1,3 @@
+ppt_data <- read.csv("gainesville-precip.csv", header = FALSE)
+monthly_mean_ppt <- colMeans(ppt_data)
+plot(monthly_mean_ppt, type = "l", xlab = "Month", ylab = "Mean Precipitation")
